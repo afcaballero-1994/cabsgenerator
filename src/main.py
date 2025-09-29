cb589d4694bc66a740cb0818b64f48438e2fbca6
= <=>
2025-09-29 15:02:43 -0500

    implemented generating html from markdown, missing completing generating content recursive

--- src/main.py
+++ src/main.py
@@ -1,8 +1,65 @@
 from textnode import TextNode
 from textnode import TextType
+from markdown_to_blocks import markdown_to_html_node
+
+import os, shutil
+
+def extrac_title(markdown: str) -> str:
+
+    sp = markdown.split("\n")
+
+    if sp[0].startswith("# "):
+        return sp[0][2:]
+    else:
+        raise ValueError("Not valid header file")
+
+def generate_page(from_path: str, template_path: str, dst_path: str):
+    print(f"generating from: {from_path} using: {template_path} to: {dst_path}")
+
+    with open(from_path) as mark, open(template_path) as templ, open(dst_path, "w") as ds:
+        markdown = mark.read()
+        templ_html = templ.read()
+        html_node = markdown_to_html_node(markdown)
+
+        title = extrac_title(markdown)
+        print(2, title)
+
+        htmtow = templ_html.replace("{{ Content }}", html_node.to_html()).replace("{{ Title }}", title)
+        ds.write(htmtow)
+
+def generate_pages_recuersive(dir_path_content: str, template_path: str, dst_dir_path: str):
+    
+
+def copy_pub():
+    src: str = "./static"
+    dst: str = "./public"
+    if os.path.exists(dst):
+        print(f"deleting: {dst}")
+        shutil.rmtree(dst)
+
+    print(f"creating dir: {dst}")
+    os.mkdir(dst)
+
+    def loop(dirs: list[str], c: str):
+        cd = list(map(lambda x: os.path.join(c, x), dirs))
+        
+        for d in cd:
+            if os.path.isfile(d):
+                print(f"about to copy: {d}")
+                cpath = d.replace(src, dst)
+                shutil.copy(d, cpath)
+            else:
+                dsc = dst + d[len(src):]
+                if not os.path.exists(dsc):
+                    print(f"creating: {dsc}")
+                    os.mkdir(dsc)
+                loop(os.listdir(d), d)
+
+
+    loop(os.listdir(src), src)
 
 def main() -> None:
-    new = TextNode("This is some anchor text", TextType.LINK, "boot.dev")
-    print(new)
+    copy_pub()
+    generate_page("./content/index.md", "./template.html", "./public/index.html")
 
 main()
